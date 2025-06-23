9909a5a97e70d14ec30b25545f1597f590f2f016
SmirkClassCarry6J <replicacrow@gmail.com>
2025-06-23 12:23:39 +0900

    update file list

--- generate_file_list.py
+++ generate_file_list.py
@@ -9,7 +9,7 @@ def scan_directory(path=""):
     
     if os.path.isdir(abs_path):
         items = []
-        for item in sorted(os.listdir(abs_path)):  # Sort alphabetically
+        for item in sorted(os.listdir(abs_path)):  # Sort alphabetically, list folders / files
             item_path = os.path.join(abs_path, item)
             if os.path.isdir(item_path):
                 # If it's a folder, recurse
@@ -17,7 +17,7 @@ def scan_directory(path=""):
                     "name": item,
                     "type": "folder",
                     "items": scan_directory(os.path.join(path, item))["items"]
-                })
+                }) # Append to a dictionary
             else:
                 # If it's a file, just add it
                 items.append({"name": item, "type": "file"})
@@ -29,7 +29,7 @@ def scan_directory(path=""):
 def generate_json():
     """Generates the file structure and saves it as file-list.json."""
     file_structure = scan_directory("")
-    with open("file-list.json", "w", encoding="utf-8") as f:
+    with open("./datasets/file-list.json", "w", encoding="utf-8") as f:
         json.dump(file_structure, f, indent=2)
 
 generate_json()
